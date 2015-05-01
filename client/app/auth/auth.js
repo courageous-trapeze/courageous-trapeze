3dbc249d54097e1e9d5d1727fd090be024dace24
Michael Harris <mwharris333@gmail.com>
2015-05-01 15:08:30 -0700

    Added auth controller and signup view

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,4 +1,35 @@
-angular.module('courageousTrapeze.auth', [])
+console.log('auth.js running...');
+var app = angular.module('courageousTrapeze.auth', []);
+app.controller('AuthController', ['$scope', 'Auth', function($scope, Auth){
+  // var test = Counter();
+  $scope.display = 'abcasdfd';
 
-.controller('AuthController', ['$scope', 'Auth', function($scope, Auth) {
+  $scope.test = function(count){
+    Auth.testGet(count, function(respCount){
+      console.log('$scope.increment() return value:', respCount);
+      $scope.display = 'aaaa';
+    })
+  };
+
 }]);
+
+app.factory('Auth', function($http){
+  console.log('Auth factory() running...');
+  var testGet = function(count, cb){
+    console.log('Auth factory() testGet() running...');
+    return $http({
+      method: 'GET',
+      url: '/signup'
+    })
+    .then(function (resp) {
+      console.log('Auth factory() testGet() response callback running...');
+      console.log('response:', resp);
+    });
+    return 44;
+  };
+
+  return {
+    testGet: testGet
+  }
+
+});
